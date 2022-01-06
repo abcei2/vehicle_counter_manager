44b3c0e8295b3deb552235215031a5cbc53d2bbd
abcei2 <santiago.salgado.duque@gmail.com>
2022-01-06 10:14:23 -0500

    dockerizing this ***, adding postgres

--- vehicle_counter_manager/settings.py
+++ vehicle_counter_manager/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'vehicle_counter_manager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
