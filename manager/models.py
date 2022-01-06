7c4a247b6978caf0fd0df4e5e0b1c2f72d302f03
abcei2 <santiago.salgado.duque@gmail.com>
2022-01-06 10:14:50 -0500

    building models

--- manager/models.py
+++ manager/models.py
@@ -1,52 +1,55 @@
 from django.db import models
-
+from django.contrib.postgres.fields import ArrayField
 # Create your models here.
 
 
 class Detection(models.Model):
-    id  = id
+
+    pieces = ArrayField(ArrayField(models.IntegerField()))
     track_id = track_id
-    class_id = class_id
-    first_bbox = first_bbox
-    last_bbox = first_bbox
-    dist_btw_bbox = 0
+    class_id = models.IntegerField()
+    first_bbox = models.ArrayField() first_bbox
+    last_bbox = ArrayField(ArrayField(models.IntegerField()))
+    dist_btw_bbox = models.IntegerField()
     first_image = image.copy()        
     last_image = image.copy()    
-    input_zone = input_zone
-    output_zone = -1
-    frames_counter = 0        
-    last_frame_detection_id = 0
-    is_lost = False
-    frames_counter_class={
-        0:{
-            "name":"person",
-            "frames_detected":0
-        },
-        1:{
-            "name":"bicycle",
-            "frames_detected":0
-        },
-        3:{
-            "name":"motorcylcle",
-            "frames_detected":0
-        },
-        2:{
-            "name":"car",
-            "frames_detected":0
-        },
-        5:{
-            "name":"bus",
-            "frames_detected":0
-        },
-        7:{
-            "name":"truck",
-            "frames_detected":0
-        }           
-    }
+    input_zone = models.IntegerField(default = -1)
+    output_zone = models.IntegerField(default = -1)
+    frames_counter = models.IntegerField(default = 0)        
+    last_frame_detection_id = models.IntegerField(default = 0)   
+    is_lost = models.BooleanField()
+    frames_counter_class= models.JSONField() 
 
-    self.detection_time = datetime.datetime.now()
-    self.last_detection_time = None  
-    x_1=models.FloatField()
-    y_1=models.FloatField()
-    x_2=models.FloatField()
-    y_2=models.FloatField()
+    detection_time = models.TimeField(auto_now=True)
+    last_detection_time = models.TimeField()  
+
+
+class AfarmentData(models.Model):
+    ammount = models.IntegerField()
+    maneuver = models.CharField()
+    class_name = models.CharField()
+    class_id = models.IntegerField()
+        
+DEFAULT_POLY = {
+    "p1":{
+        "point":[50 ,50],
+        "pressed":False
+    },            
+    "p2":{
+        "point":[50 ,100],
+        "pressed":False
+    },            
+    "p3":{
+        "point":[100 ,100],
+        "pressed":False
+    },            
+    "p4":{
+        "point":[100 ,50],
+        "pressed":False
+    }
+}
+class ZoneConfig(models.Model):
+    
+    frames_counter_class= models.JSONField() 
+    zones = ArrayField(models.JSONField(default=DEFAULT_POLY) )
+ 
